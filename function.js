e9d10c7b04f177a296afcc5a6c78174b27cf3be2
Adwaith V <adwaithv964@gmail.com>
2024-11-06 22:49:30 +0530

    Ai Conclave

---
+++ function.js
@@ -0,0 +1,4 @@
+// Example JavaScript to handle button click or any future interactive elements
+document.querySelector('.browse-btn').addEventListener('click', () => {
+    alert('Browse topics button clicked!');
+});
